75a15e4233b3662cff961390986174437883c279
Mathews96 <mathewsfreire@gmail.com>
2022-11-13 10:19:11 -0300

    1.0.1 - Frm(Inicial, Agendamento)

--- VIEW/Form1.Designer.cs
+++ VIEW/Form1.Designer.cs
@@ -28,13 +28,133 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.pctBoxLogo = new System.Windows.Forms.PictureBox();
+            this.lbLinksUteis = new System.Windows.Forms.Label();
+            this.pnlLinksUteis = new System.Windows.Forms.Panel();
+            this.btnCadmutLinkUteis = new System.Windows.Forms.Button();
+            this.btnNissLinkUteis = new System.Windows.Forms.Button();
+            this.btnAgendar = new System.Windows.Forms.Button();
+            this.btnCadastrar = new System.Windows.Forms.Button();
+            this.btnConsultar = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pctBoxLogo)).BeginInit();
+            this.pnlLinksUteis.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // pctBoxLogo
+            // 
+            this.pctBoxLogo.BackColor = System.Drawing.SystemColors.MenuText;
+            this.pctBoxLogo.Image = ((System.Drawing.Image)(resources.GetObject("pctBoxLogo.Image")));
+            this.pctBoxLogo.Location = new System.Drawing.Point(12, 12);
+            this.pctBoxLogo.Name = "pctBoxLogo";
+            this.pctBoxLogo.Size = new System.Drawing.Size(285, 96);
+            this.pctBoxLogo.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pctBoxLogo.TabIndex = 0;
+            this.pctBoxLogo.TabStop = false;
+            // 
+            // lbLinksUteis
+            // 
+            this.lbLinksUteis.AutoSize = true;
+            this.lbLinksUteis.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbLinksUteis.ForeColor = System.Drawing.SystemColors.HotTrack;
+            this.lbLinksUteis.Location = new System.Drawing.Point(111, 133);
+            this.lbLinksUteis.Name = "lbLinksUteis";
+            this.lbLinksUteis.Size = new System.Drawing.Size(84, 13);
+            this.lbLinksUteis.TabIndex = 1;
+            this.lbLinksUteis.Text = "LINKS ÚTEIS";
+            // 
+            // pnlLinksUteis
+            // 
+            this.pnlLinksUteis.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.pnlLinksUteis.Controls.Add(this.btnNissLinkUteis);
+            this.pnlLinksUteis.Controls.Add(this.btnCadmutLinkUteis);
+            this.pnlLinksUteis.Location = new System.Drawing.Point(12, 149);
+            this.pnlLinksUteis.Name = "pnlLinksUteis";
+            this.pnlLinksUteis.Size = new System.Drawing.Size(285, 53);
+            this.pnlLinksUteis.TabIndex = 2;
+            // 
+            // btnCadmutLinkUteis
+            // 
+            this.btnCadmutLinkUteis.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnCadmutLinkUteis.Location = new System.Drawing.Point(15, 8);
+            this.btnCadmutLinkUteis.Name = "btnCadmutLinkUteis";
+            this.btnCadmutLinkUteis.Size = new System.Drawing.Size(84, 33);
+            this.btnCadmutLinkUteis.TabIndex = 3;
+            this.btnCadmutLinkUteis.Text = "CADMUT";
+            this.btnCadmutLinkUteis.UseVisualStyleBackColor = true;
+            // 
+            // btnNissLinkUteis
+            // 
+            this.btnNissLinkUteis.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnNissLinkUteis.Location = new System.Drawing.Point(185, 8);
+            this.btnNissLinkUteis.Name = "btnNissLinkUteis";
+            this.btnNissLinkUteis.Size = new System.Drawing.Size(84, 33);
+            this.btnNissLinkUteis.TabIndex = 4;
+            this.btnNissLinkUteis.Text = "NISS";
+            this.btnNissLinkUteis.UseVisualStyleBackColor = true;
+            // 
+            // btnAgendar
+            // 
+            this.btnAgendar.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnAgendar.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.btnAgendar.Location = new System.Drawing.Point(12, 228);
+            this.btnAgendar.Name = "btnAgendar";
+            this.btnAgendar.Size = new System.Drawing.Size(285, 62);
+            this.btnAgendar.TabIndex = 3;
+            this.btnAgendar.Text = "AGENDAR";
+            this.btnAgendar.UseVisualStyleBackColor = true;
+            // 
+            // btnCadastrar
+            // 
+            this.btnCadastrar.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnCadastrar.Location = new System.Drawing.Point(12, 296);
+            this.btnCadastrar.Name = "btnCadastrar";
+            this.btnCadastrar.Size = new System.Drawing.Size(285, 62);
+            this.btnCadastrar.TabIndex = 4;
+            this.btnCadastrar.Text = "CADASTRAR";
+            this.btnCadastrar.UseVisualStyleBackColor = true;
+            // 
+            // btnConsultar
+            // 
+            this.btnConsultar.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnConsultar.Location = new System.Drawing.Point(12, 364);
+            this.btnConsultar.Name = "btnConsultar";
+            this.btnConsultar.Size = new System.Drawing.Size(285, 62);
+            this.btnConsultar.TabIndex = 5;
+            this.btnConsultar.Text = "CONSULTAR";
+            this.btnConsultar.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(309, 450);
+            this.Controls.Add(this.btnConsultar);
+            this.Controls.Add(this.btnCadastrar);
+            this.Controls.Add(this.btnAgendar);
+            this.Controls.Add(this.pnlLinksUteis);
+            this.Controls.Add(this.lbLinksUteis);
+            this.Controls.Add(this.pctBoxLogo);
+            this.Name = "Form1";
             this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pctBoxLogo)).EndInit();
+            this.pnlLinksUteis.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.PictureBox pctBoxLogo;
+        private System.Windows.Forms.Label lbLinksUteis;
+        private System.Windows.Forms.Panel pnlLinksUteis;
+        private System.Windows.Forms.Button btnNissLinkUteis;
+        private System.Windows.Forms.Button btnCadmutLinkUteis;
+        private System.Windows.Forms.Button btnAgendar;
+        private System.Windows.Forms.Button btnCadastrar;
+        private System.Windows.Forms.Button btnConsultar;
     }
 }
 
